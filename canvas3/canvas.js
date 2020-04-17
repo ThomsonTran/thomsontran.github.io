fc1f188d9ed137e3316c1de0bafffc4073c00f9a
Thomson Tran <thomson.tran@gmail.com>
2020-04-17 02:12:27 -0400

    first commit

---
+++ canvas3/canvas.js
@@ -0,0 +1,180 @@
+const canvas = document.getElementById("canvas1");
+const c = canvas.getContext("2d");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+let mouse = {
+    x: undefined,
+    y: undefined,
+    radius: 10,
+};
+
+let color = "white";
+let particles = [];
+let smallParticles = [];
+let timer = 0;
+
+function randomIntBetween(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+class Particle {
+    constructor(x, y, speedx, speedy, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.speed = {
+            x: speedx,
+            y: speedy,
+        };
+        this.radius = radius;
+        this.color = color;
+        this.gravity = 1;
+        this.friction = 0.8;
+    }
+
+    break() {
+        this.radius = Math.max(0, this.radius - 3);
+        for (let index = 0; index < 8; index++) {
+            let smallradius = this.radius / 4;
+            let speedx = randomIntBetween(-15, 15);
+            let speedy = randomIntBetween(-15, 15);
+            smallParticles.push(
+                new SmallParticle(
+                    this.x,
+                    this.y,
+                    speedx,
+                    speedy,
+                    smallradius,
+                    "white"
+                )
+            );
+        }
+    }
+
+    draw() {
+        c.save();
+        c.beginPath();
+        c.arc(this.x, this.y, Math.abs(this.radius), 0, Math.PI * 2, false);
+
+        c.shadowColor = "#E3EAEF";
+        c.shadowBlur = 20;
+        c.shadowOffsetX = 0;
+        c.shadowOffsetY = 0;
+
+        c.fillStyle = "#E3EAEF";
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+
+    update() {
+        if (
+            this.x + this.radius + this.speed.x >= canvas.width ||
+            this.x - this.radius + this.speed.x <= 0
+        ) {
+            this.speed.x = -this.speed.x;
+            this.break();
+        }
+        if (this.y + this.radius + this.speed.y >= canvas.height) {
+            this.speed.y = -this.speed.y * this.friction;
+            this.break();
+        } else {
+            this.speed.y += this.gravity;
+        }
+        this.y += this.speed.y;
+        this.x += this.speed.x * 0.9;
+        this.draw();
+    }
+}
+
+class SmallParticle extends Particle {
+    constructor(x, y, speedx, speedy, radius, color) {
+        super(x, y, speedx, speedy, radius, color);
+        this.gravity = randomIntBetween(0.1, 0.8);
+        this.opacity = 1;
+    }
+    draw() {
+        c.save();
+        c.beginPath();
+        c.arc(this.x, this.y, Math.abs(this.radius), 0, Math.PI * 2, false);
+        c.globalAlpha = this.opacity;
+        c.shadowColor = "#E3EAEF";
+        c.shadowBlur = 20;
+        c.shadowOffsetX = 0;
+        c.shadowOffsetY = 0;
+
+        c.fillStyle = "#E3EAEF";
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+
+    update() {
+        if (this.y + this.radius + this.speed.y >= canvas.height) {
+            this.speed.y = -this.speed.y * this.friction;
+        } else {
+            this.speed.y += this.gravity;
+        }
+        this.x += this.speed.x;
+        this.y += this.speed.y;
+        this.decreaseOpacity();
+        this.draw();
+    }
+
+    decreaseOpacity() {
+        this.opacity -= 0.01;
+        if (this.opacity <= 0) {
+            this.opacity = 0;
+        }
+    }
+}
+
+function init() {
+    let x = randomIntBetween(50, canvas.width - 50);
+
+    particles.push(new Particle(x, 0, 10, 20, 15, color));
+}
+
+var backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
+// backgroundGradient.addColorStop(0, "#171e26");
+// backgroundGradient.addColorStop(1, "#3f586b"); 171e26
+
+// backgroundGradient.addColorStop(0, "#101a3b");
+// backgroundGradient.addColorStop(1, "#363a6a");
+
+backgroundGradient.addColorStop(0, "#0b2139");
+backgroundGradient.addColorStop(1, "#101a3b");
+function animate() {
+    timer++;
+
+    if (timer % 200 == 0) {
+        init();
+    }
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = backgroundGradient;
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((particle, index) => {
+        if (particle.radius <= 0) {
+            particles.splice(index, 1);
+        }
+        particle.update();
+    });
+
+    smallParticles.forEach((particle, index) => {
+        particle.update();
+
+        if (particle.opacity <= 0) {
+            smallParticles.splice(index, 1);
+        }
+    });
+}
+
+init();
+animate();
---
+++ canvas3/main.js
@@ -0,0 +1,111 @@
+const canvas = document.getElementById("scene");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+    x: undefined,
+    y: undefined,
+};
+
+const colorsArray = ["#E68E99", "#7888E6", "#E8C75D", "#6CE686", "#6074E6"];
+let circlesArray = [];
+
+// global functions
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colorsArray.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
+
+// Event Listeners
+addEventListener("mousemove", (event) => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+    init();
+});
+addEventListener(
+    "touchmove",
+    (event) => {
+        event.preventDefault();
+        let touch = event.touches[0];
+        mouse.x = touch.clientX;
+        mouse.y = touch.clientY;
+        init();
+    },
+    { passive: false }
+);
+
+addEventListener("resize", () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+});
+
+// Objects
+class Circle {
+    constructor(x, y, dx, dy, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.radius -= 0.2;
+        this.radius = Math.max(0, this.radius);
+        this.draw();
+    }
+}
+
+// Implementation
+function init() {
+    let x;
+    let y;
+    let radius;
+    let color;
+    let dx = 0;
+    let dy;
+
+    for (let i = 0; i < 2; i++) {
+        x = mouse.x + randomIntFromRange(-1, 1) * randomIntFromRange(1, 50);
+        y = mouse.y + randomIntFromRange(-1, 1) * randomIntFromRange(1, 50);
+        dy = randomIntFromRange(1, 5);
+        dx = randomIntFromRange(1, 5);
+        radius = randomIntFromRange(1, 25);
+        color = randomColor(colorsArray);
+        circlesArray.push(new Circle(x, y, dx, dy, radius, color));
+    }
+}
+
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    circlesArray.forEach((circle) => {
+        circle.update();
+    });
+}
+
+init();
+animate();
